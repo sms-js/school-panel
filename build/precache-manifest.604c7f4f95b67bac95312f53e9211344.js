b967c16ed433251d574a4dc42c9e435196caa026
Pablo Gambetta <gambeita@gmail.com>
2019-05-07 20:47:36 -0300

    v0.4.0 Build

---
+++ build/precache-manifest.604c7f4f95b67bac95312f53e9211344.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "38bf6ee5820d8a55a58a",
+    "url": "/static/css/main.54a78204.chunk.css"
+  },
+  {
+    "revision": "38bf6ee5820d8a55a58a",
+    "url": "/static/js/main.38bf6ee5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "aa1e055ab602a357ec42",
+    "url": "/static/css/2.3c8f7aa3.chunk.css"
+  },
+  {
+    "revision": "aa1e055ab602a357ec42",
+    "url": "/static/js/2.aa1e055a.chunk.js"
+  },
+  {
+    "revision": "98101fa32d171749046022ebc8809076",
+    "url": "/index.html"
+  }
+];
