f04a70e2a9e20385c1c493e694245e62a207788f
alexisschapiro <proyecto.chaca@gmail.com>
2019-11-10 10:37:20 -0300

    drawer container menu: minor fix

--- src/components/DrawerContainer/DrawerContent.js
+++ src/components/DrawerContainer/DrawerContent.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
@@ -7,18 +7,20 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import HomeIcon from 'components/icons/HomeIcon';
-import FaceIcon from 'components/icons/FaceIcon';
 import MessageIcon from 'components/icons/MessageIcon';
 import Divider from '@material-ui/core/Divider';
 import Collapse from '@material-ui/core/Collapse';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
-import GroupIcon from 'components/icons/GroupIcon';
+import GroupIcon from 'components/icons/GroupIcon'; //SVG
+import FaceIcon from 'components/icons/FaceIcon'; //SVG
+import AddStudentIcon from 'components/icons/AddStudentIcon';
 import GroupAddIcon from '@material-ui/icons/GroupAdd';
+import { blue } from '@material-ui/core/colors';
 
 const DrawerContent = classes => {
-	const [open, setOpen] = useState(false);
+	const [toggle, setToggle] = useState({ groups: false, data: false });
 
 	const useStyles = makeStyles(theme => ({
 		root: {
@@ -33,9 +35,12 @@ const DrawerContent = classes => {
 	}));
 	const nestedListClasses = useStyles();
 
-	function handleClick() {
-		setOpen(!open);
+	function handleToggle(name) {
+		const newToggle = { ...toggle };
+		newToggle[name] = !newToggle[name];
+		setToggle(newToggle);
 	}
+
 	return (
 		<div>
 			<div className={classes.toolbar} />
@@ -52,14 +57,14 @@ const DrawerContent = classes => {
 					<MessageIcon />
 					<ListItemText primary="Messages" />
 				</ListItem>
-				<ListItem key="group" button onClick={handleClick}>
+				<ListItem key="groups" button onClick={() => handleToggle('groups')}>
 					<ListItemIcon>
 						<GroupIcon />
 					</ListItemIcon>
 					<ListItemText primary="Groups" />
-					{open ? <ExpandLess /> : <ExpandMore />}
+					{toggle.groups ? <ExpandLess /> : <ExpandMore />}
 				</ListItem>
-				<Collapse in={open} timeout="auto" unmountOnExit>
+				<Collapse in={toggle.groups} timeout="auto" unmountOnExit>
 					<List component="div" disablePadding>
 						<ListItem
 							key="generateGroup"
@@ -75,29 +80,21 @@ const DrawerContent = classes => {
 						</ListItem>
 					</List>
 				</Collapse>
-
-				<ListItem key="data" button onClick={handleClick}>
+				<ListItem key="data" button onClick={() => handleToggle('data')}>
 					<ListItemIcon>
 						<GroupIcon />
 					</ListItemIcon>
 					<ListItemText primary="Data" />
-					{open ? <ExpandLess /> : <ExpandMore />}
+					{toggle.data ? <ExpandLess /> : <ExpandMore />}
 				</ListItem>
-				<Collapse in={open} timeout="auto" unmountOnExit>
+				<Collapse in={toggle.data} timeout="auto" unmountOnExit>
 					<List component="div" disablePadding>
 						<ListItem component={Link} to="/admin/users" key="users">
 							<FaceIcon />
 							<ListItemText primary="Users" />
 						</ListItem>
-						<ListItem
-							key="addStudentData"
-							button
-							className={nestedListClasses.nested}
-							component={Link}
-							to="/data/add">
-							<ListItemIcon>
-								<GroupAddIcon color="primary" />
-							</ListItemIcon>
+						<ListItem component={Link} to="/data/add" key="addStudentPersonalData">
+							<AddStudentIcon color="secondary" />
 							<ListItemText primary="Add student" />
 						</ListItem>
 					</List>
